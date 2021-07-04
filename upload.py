91ead42639884dc4dacf166bf563c57ebd79d92b
Paul Schellnast <paulschellnast@gmail.com>
2021-07-04 22:56:20 +0200

    second commit

--- upload.py
+++ upload.py
@@ -1,6 +1,20 @@
 import requests
+import json
 url = 'https://api.anonfiles.com/upload'
-filepath = input("Enter filepath:")
-print("filepath is: " + filepath)
-x = requests.post(url, files = {'file': open(filepath,'rb')},)
-print("DOWNLOAD LINK: " + x.json()["data"]["file"]["url"]["full"])
+print("1: Getting information from a file")
+print("2: Upload a file")
+y = input("Choose option: ")
+if y == "2":
+    filepath = input("Enter filepath:")
+    print("filepath is: " + filepath)
+    x = requests.post(url, files = {'file': open(filepath,'rb')},)
+    print("DOWNLOAD LINK: " + x.json()["data"]["file"]["url"]["full"])
+if y == "1":
+    fileinfo = input("Enter file ID: ")
+    print('https://api.anonfiles.com/v2/file/'+fileinfo+'/info')
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.124 Safari/537.36',
+    'From': 'petersars@gmail.com'  
+    }
+    z = requests.get('https://api.anonfiles.com/v2/file/'+fileinfo+'/info', headers=headers)
+    print(z.json())
